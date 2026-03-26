bb867aa44598d7350e2ff880d957fc89eda8147c
Juan_Carlos <reynosozunigajuancarlos@outlook.com>
2026-03-26 02:29:09 -0600

    Configuracion de la conexion MYSQL y Configuracion de los Modelos

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# Tabla de Menús
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+
+# Tabla de Reservas
+class Booking(models.Model):
+    first_name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.first_name
