7aef0c7822b8af024f7acc73896d0efe092907a8
Juan Carlos Reynoso Zuñiga <116603804+Juan01232323@users.noreply.github.com>
2026-03-29 19:15:42 -0600

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-# Create your models here.
 
-# Tabla de Menús
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -11,11 +9,12 @@ class Menu(models.Model):
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
 
-# Tabla de Reservas
 class Booking(models.Model):
     first_name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
-    booking_date = models.DateTimeField()
+    booking_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
 
     def __str__(self):
         return self.first_name
+
