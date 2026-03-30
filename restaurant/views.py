84739a91e71f23d8b4a9afd0da0e06344d2abd07
Juan Carlos Reynoso Zuñiga <116603804+Juan01232323@users.noreply.github.com>
2026-03-29 20:05:02 -0600

    Update views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Menu, Booking  
-from .serializers import MenuSerializer, BookingSerializer
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from rest_framework import generics, viewsets 
 from rest_framework.permissions import IsAuthenticated
 from django.http import HttpResponse 
@@ -8,9 +8,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
-from django.core import serializers
+from django.core import serializers  
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
+from django.contrib.auth.models import User
+
 
 def index(request):
     return render(request, 'index.html', {})
@@ -39,15 +41,15 @@ def about(request):
 @csrf_exempt 
 def book(request):
     if request.method == 'POST':
-        data = json.loads(request.body) 
+        data = json.loads(request.body) # Cambia json.load por json.loads(request.body)
         
         booking = Booking(
             first_name=data['first_name'],
-            booking_date=data['reservation_date'],
-            no_of_guests=data.get('no_of_guests', 1) 
+            booking_date=data['reservation_date'], # 'reservation_date' del form va a 'booking_date' del modelo
+            no_of_guests=data.get('no_of_guests', 1) # Si el form no tiene invitados, ponemos 1 por defecto
         )
         booking.save()
-        return JsonResponse({"status": "success"}) 
+        return JsonResponse({"status": "success"}) # Es buena práctica retornar un JSON en un POST de este tipo
         
     return render(request, 'book.html')
 
@@ -61,6 +63,7 @@ def bookings(request):
     if date:
         bookings_data = Booking.objects.filter(booking_date=date)
         data = list(bookings_data.values())
+        # Usamos el codificador de Django aquí para que no falle con las fechas
         return JsonResponse(data, safe=False, encoder=DjangoJSONEncoder)
     else:
         bookings_data = Booking.objects.all()
@@ -82,3 +85,7 @@ def display_menu_item(request, pk=None):
     else: 
         menu_item = "" 
     return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+
+class VistaUsuarios(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
